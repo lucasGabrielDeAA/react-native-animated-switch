55db12b4c2d712f3054cc2752a74da015855fde9
lucasGabrielDeAA <lg.technus@gmail.com>
2020-09-04 14:21:28 -0300

    colors changed added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import {Container, Title} from './styles';
 
@@ -9,18 +9,32 @@ import {useTheme} from '../../hooks/useTheme';
 const Home: React.FC = () => {
   const {toggleTheme, theme} = useTheme();
 
+  const [activeTrackColor, setActiveTrackColor] = useState(
+    theme === 'dark' ? '#fa0' : '#aaa',
+  );
+  const [inactiveTrackColor, setInactivectiveTrackColor] = useState(
+    theme === 'dark' ? '#aaa' : '#fa0',
+  );
+
+  useEffect(() => {
+    setActiveTrackColor(theme === 'dark' ? '#fa0' : '#aaa');
+    setInactivectiveTrackColor(theme === 'dark' ? '#aaa' : '#fa0');
+  }, [theme]);
+
   return (
     <Container>
       <Title>
-        {theme === 'dark' ? 'Join the dark side' : 'The light hurts my eyes'}
+        {theme === 'dark'
+          ? 'Join the dark side'
+          : 'Please turn off! The light hurts my eyes'}
       </Title>
 
       <Switch
-        handleOnPress={toggleTheme}
-        activeTrackColor="#007Aff"
-        inactiveTrackColor="#f2f5f7"
+        activeTrackColor={activeTrackColor}
+        inactiveTrackColor={inactiveTrackColor}
         thumbColor="#fff"
         value={theme === 'dark'}
+        handleOnPress={toggleTheme}
       />
     </Container>
   );
